9873635500b6b5a63d5b9353a0d9f57232d35a47
kuanghc1 <kuanghc1@lenovo.com>
2023-06-20 14:38:56 +0800

    add comments

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -4,6 +4,10 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
+    //   router最外层，包裹整个app
+    //   router内部，包裹需要被router管理的部分
+    //   switch内部，包裹route
+    //   route内部，包裹需要被route管理的部分
     <Router>
       <div className="App">
         <Navbar />
